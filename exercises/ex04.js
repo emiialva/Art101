a4924d1e2277d53c2be958f67eb76aaad1703254
emisalva <emisalva@ucsc.edu>
2025-10-14 18:49:37 -0700

    vhs

---
+++ exercises/ex04.js
@@ -0,0 +1,29 @@
+
+let totalNumber= 0;  
+
+
+ // add a button titled click me  
+ $("#needy-button").html("new text"); 
+  
+ $("#needy-button").click(function(){
+    totalNumber = totalNumber +3;   
+     
+    let sentence ="I was clicked oh no ";  
+    let sentenceTwo =" oh no"; 
+    let totalNumbers= sentence + totalNumber 
+    +sentenceTwo;
+    
+
+    $("#needy-button").html(totalNumbers);
+ }); 
+
+// when it is clicked  
+//add one to total number   
+ 
+//show the total number 
+// on our button 
+//show "clicked total number times" 
+
+
+// a top limit
+
