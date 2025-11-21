e2d21c9f007afe1274a4ca20fe5b0335329c0f63
emisalva <emisalva@ucsc.edu>
2025-11-20 19:25:49 -0800

    new

--- garden/creature.js
+++ garden/creature.js
@@ -1,31 +1,163 @@
-$("#add-creature").click(
-    function () {
-
-        // grab the value from the input
-        let crName = $("#crName").val();
-        let crColor= $("#crColor").val();
-        let crEyesNum= $("#crEyesNum").val();
-        let crEyesHTML="";
-
-        for (let i = 0; i < crEyesNum ; i++){
-            crEyesHTML=crEyesHTML + "<div class=eye>.</div>";
-        }
 
 
-        // check for the field value do not add empty ones
-        if ( (crName == "") || (crName.length>12) ) { // do nothing 
-        }
-        else {
-            $("#creature-list").append(`
+// main add click handler
+  // create creature object based on form inputs
+  // safety check-ups
+  // prepare an html for a single creature
+ 
+  // print single creature on a page
+  // add the creature to the memory
+  // reset the from
+
+// memorize all creatures in array
+let allCreatures = [];
+
+// random name api
+async function getRandomName() {
+
+   const response = await fetch(
+"https://api.gofakeit.com/funcs/petname", 
+{method: "GET",});
+
+   const nameRandom = await response.text();
+   console.log("Got name:", nameRandom);
+   return nameRandom;
+}
+
+// random name api
+async function getRandomColor() {
+
+   const response = await fetch(
+"https://api.gofakeit.com/funcs/hexcolor", 
+{method: "GET",});
+
+   const nameRandom = await response.text();
+   console.log("Got name:", nameRandom);
+   return nameRandom;
+}
+
+// random emoji api
+async function getRandomEmoji() {
+
+   const response = await fetch(
+"https://api.gofakeit.com/funcs/emojianimal", 
+{method: "GET",});
+
+   const nameRandom = await response.text();
+   console.log("Got name:", nameRandom);
+   return nameRandom;
+}
+
+// random creature
+async function randomizeCreature() {
+
+   const eyesRandom = Math.floor(Math.random() * 5) + 1;
+   const nameRandom = await getRandomName();
+   const colorRandom = await getRandomColor();
+   const emojiRandom=await getRandomEmoji();
+
+   const randomCreature = {
+       name: emojiRandom+nameRandom,
+       color: colorRandom,
+       eyesNum: eyesRandom
+   };
+
+   return randomCreature;
+}
+
+// grabs data from the form
+function getCreatureFromForm() {
+    const freshCreature = {
+        name: $("#crName").val(),
+        color: $("#crColor").val(),
+        eyesNum: $("#crEyesNum").val()
+    };
+
+    return freshCreature;
+};
+
+// prepare HTML for a single creature (does NOT add to the page)
+function renderCreature(creature) {
+  let crEyesHTML = "";
+
+  for (let i = 0; i < creature.eyesNum; i++) {
+    crEyesHTML = crEyesHTML + "<div class='eye'>.</div>";
+  }
+
+  const html=`
 <div class="creature">
-    <div class="creature-body" style="background: ${crColor}">${crEyesHTML}</div>
-    <div class="creature-info">${crName}</div>
+  <div class="creature-body" style="background: ${creature.color}">
+${crEyesHTML}
+  </div>
+  <div class="creature-info">${creature.name}</div>
 </div>
-`);
-        }
+`;
+  return html;
+}
 
-        // remove the name after it's added
-        $("#crName").val("");
 
-    });
-    
+// append one creature to the DOM using its HTML
+function addCreatureToDOM(creature) {
+  html = renderCreature(creature);
+  $("#creature-list").append(html);
+
+}
+
+// check if creature data is valid
+function isCreatureValid(creature) {
+  if (creature.name === "") return false;
+  if (creature.name.length > 12) return false;
+  if (creature.eyesNum=="" || creature.eyesNum > 5) return false;
+  return true;  
+}
+
+// clear all form fields
+function clearForm() {
+  $("#crName").val("");
+  $("#crColor").val("#ee7dea");  // or keep previous if you prefer
+  $("#crEyesNum").val(1);
+}
+
+
+
+// BUTTON HANDLERS
+
+$("#add-creature").click(
+    async function () {
+
+ 
+    let newCreature;
+
+   // choose the source for a creature
+    if( $("#crRandom").is(':checked') ) 
+    { newCreature= await randomizeCreature();}
+    else 
+    { newCreature= getCreatureFromForm(); }
+
+    // do checks, exit function if something is wrong
+   if ( isCreatureValid(newCreature)==false) {
+    return;
+   }
+   
+   allCreatures.push(newCreature);   // remember it
+   addCreatureToDOM(newCreature);    // show it
+   clearForm();
+
+    }); 
+
+$("#shuffle-creatures").click(
+function (){
+  renderAllCreaturesRandom();
+
+}); 
+ 
+// grabs data from the form
+function getCreatureFromForm() {
+    const freshCreature = {
+        name: $("#crName").val(),
+        color: $("#crColor").val(),
+        eyesNum: $("#crEyesNum").val()
+    };
+
+    return freshCreature;
+};
---
+++ garden/creatureog.js
@@ -0,0 +1,29 @@
+// a variable for storing names 
+ 
+// when the button is clicked (event)
+ 
+$("#add-creature").click(   
+    function(){    
+    // a variable for story inggnames 
+
+//reach for this input field and grab the text from it
+      //grab hte value from input
+let crName= $("#crName").val();  
+let crColor=$("#crColor").val(); 
+let crEyesNum=$("#crEyesNum").val();
+ 
+//check for the field value do not add empty ones 
+ 
+if(crName == "" || (crName.length>12)){// do nothing 
+    } 
+    else{  
+        $("#creature-list").append ("<div>"+crName+ ", " +crColor+ ","+crEyesNum+ "</div>");
+
+    }
+$("#creature-list").append (crName+ ", ");
+    //here our actions when the buttons is clicked
+ 
+    //remove the name after it's added  
+    $("#crName").val("");
+});
+//the magic move 
